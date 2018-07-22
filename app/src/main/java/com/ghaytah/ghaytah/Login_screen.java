91591bae5d4cdc204524c082d40f044a72367c92
awirboy <awirboy@gmail.com>
2018-07-22 18:36:00 +0400

    Setting Up

--- app/src/main/java/com/ghaytah/ghaytah/Login_screen.java
+++ app/src/main/java/com/ghaytah/ghaytah/Login_screen.java
@@ -1,13 +1,33 @@
 package com.ghaytah.ghaytah;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class Login_screen extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+    Handler mHandler = new Handler();
+    Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        mHandler.postDelayed(mRunnable, 2000); // splash time
     }
 }
