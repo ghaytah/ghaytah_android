664b1ea05555316eae1dbb83c58d26420e2dc891
awirboy <awirboy@gmail.com>
2018-07-23 12:41:10 +0400

    Setting Up

--- app/src/main/java/com/ghaytah/ghaytah/Login_screen.java
+++ app/src/main/java/com/ghaytah/ghaytah/Login_screen.java
@@ -4,18 +4,26 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class Login_screen extends AppCompatActivity {
 
-    RelativeLayout rellay1, rellay2;
+    // TODO: Add member variables here:
+
+       private RelativeLayout rellay1, rellay2;
+       private Button btn_lgn, btn_cretacct, btn_frgtpwd;
+
+
+    // TODO: Adding splash screen here:
+
     Handler mHandler = new Handler();
     Runnable mRunnable = new Runnable() {
         @Override
         public void run() {
 
-            rellay1.setVisibility(View.VISIBLE);
-            rellay2.setVisibility(View.VISIBLE);
+           rellay1.setVisibility(View.VISIBLE);
+           rellay2.setVisibility(View.VISIBLE);
 
         }
     };
@@ -26,7 +34,10 @@ public class Login_screen extends AppCompatActivity {
         setContentView(R.layout.activity_login_screen);
 
         rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
-        rellay1 = (RelativeLayout) findViewById(R.id.rellay2);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+        btn_lgn = (Button) findViewById(R.id.btn_lgn);
+        btn_cretacct = (Button) findViewById(R.id.btn_cretacct);
+        btn_frgtpwd = (Button) findViewById(R.id.btn_frgtpwd);
 
         mHandler.postDelayed(mRunnable, 2000); // splash time
     }
